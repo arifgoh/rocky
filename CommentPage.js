3cd758c42cd72cb99c143784a35ce13f05decadc
Arif Farhan Bin Annas Goh <TM36916@TM.MY>
2018-08-30 13:47:50 +0800

    push new

--- CommentPage.js
+++ CommentPage.js
@@ -1,10 +1,51 @@
 import React,{Component} from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity, View, TextInput, Keyboard} from 'react-native';
 
 export default class App extends Component {
- render() {
-   return <TouchableOpacity onPress={()=>this.props.navigation.navigate('Trending',{age:29})}>
-       <Text>Go to Trending</Text>
-     </TouchableOpacity>;
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  componentDidMount () {
+    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow);
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide);
+  }
+
+  componentWillUnmount () {
+    this.keyboardDidShowListener.remove();
+    this.keyboardDidHideListener.remove();
+  }
+
+  _keyboardDidShow () {
+    //alert('Keyboard Shown');
+  }
+
+  _keyboardDidHide () {
+    //alert('Keyboard Hidden');
+  }
+ 
+  render() {
+   
+    return ( /*<TouchableOpacity onPress={()=>this.props.navigation.navigate('Trending',{age:29})}>
+        <Text>Go to Trending</Text>
+  </TouchableOpacity>*/
+      <View style={{padding: 10}}>
+        
+        <TextInput
+          style={{height: 40}}
+          placeholder="Add Complaint"
+          onChangeText={(text) => this.setState({text})}
+        />
+
+        <TouchableOpacity style={{backgroundColor:'green'}} onPress={()=>this.props.navigation.navigate('Home',{age:29})}>
+        <Text>POST</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor:'red'}} onPress={()=>this.props.navigation.navigate('Home',{age:29})}>
+        <Text>CANCEL</Text>
+        </TouchableOpacity>
+
+  </View>);
  }
 }
--- Home.js
+++ Home.js
@@ -1,72 +1,51 @@
 import React,{Component} from 'react';
-import {Text, TouchableOpacity, View, FlatList} from 'react-native';
+import {Text, TouchableOpacity, View, FlatList, Image} from 'react-native';
 
-
-const data = [
-  {key:1,sender:'Fattah',receiver:'Myself',message:'Hi welcome to my class'},
-  {key:2,sender:'Ali',receiver:'Myself',message:'Yeah'},
-  {key:3,sender:'John',receiver:'Myself',message:'abc'},
-  {key:4,sender:'Abu',receiver:'Myself',message:'bla bla'},
-]
-
-// const database = [
-//   {username:'Fattah', date:'21/8/2018', complain:'Hi welcome to my class', noupvote:'3',nodownvote:'2',nocomment:'0'},
-//   {username:'Fattah', date:'21/8/2018', complain:'Hi welcome to my class', noupvote:'3',nodownvote:'2',nocomment:'0'},
-//   {username:'Fattah', date:'21/8/2018', complain:'Hi welcome to my class', noupvote:'3',nodownvote:'2',nocomment:'0'},
-//   {username:'Fattah', date:'21/8/2018', complain:'Hi welcome to my class', noupvote:'3',nodownvote:'2',nocomment:'0'},
-//   ]
+const database = [
+   {image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPSmB_7wJ_ypq2On_VNrLIeEAz5hnd7fKKtXhzhmpuys8ZAt6y',username:'Fattah', date:'21/8/2018', complain:'Hi welcome to my class', noupvote:'3',nodownvote:'2',nocomment:'0'},
+   {image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPSmB_7wJ_ypq2On_VNrLIeEAz5hnd7fKKtXhzhmpuys8ZAt6y',username:'Fattah', date:'21/8/2018', complain:'Hi welcome to my class', noupvote:'3',nodownvote:'2',nocomment:'0'},
+   {image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPSmB_7wJ_ypq2On_VNrLIeEAz5hnd7fKKtXhzhmpuys8ZAt6y',username:'Fattah', date:'21/8/2018', complain:'Hi welcome to my class', noupvote:'3',nodownvote:'2',nocomment:'0'},
+   {image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPSmB_7wJ_ypq2On_VNrLIeEAz5hnd7fKKtXhzhmpuys8ZAt6y',username:'Fattah', date:'21/8/2018', complain:'Hi welcome to my class', noupvote:'3',nodownvote:'2',nocomment:'0'},
+   {image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbvZyVHr1TH8UwBthEZBW8UPEPByqTp_fI8nDCU_VacMTpICWV',username:'Fiqri', date:'21/8/2018', complain:'I hate this class', noupvote:'100',nodownvote:'2',nocomment:'0'},
+   ]
 
 export default class App extends Component {
  /*componentDidMount(){
 alert(this.props.navigation.state.params.age)
  }*/
 
- keyExtractor = item => item.sender.toString();
- renderItem = ({item}) => 
-        // (<View style={{backgroundColor:'red',margin:10,height:3}}>
-        <Text>{item.key}</Text>
-          //{/* <Text>From {item.sender}</Text>
-          //<Text>To {item.receiver}</Text>
-          //<Text>Message : {item.message}</Text> */}
-        // </View>);
-
-//  renderRow = item => 
-//         (<View style={{backgroundColor:'green',margin:10}}>
-//         <Text>gggggg</Text>
-//           <Text>username: {item.username}</Text>
-//           <Text>date: {item.date}</Text>
-//           <Text>complain: {item.complain}</Text>
-//           <Text>no upvote: {item.noupvote}</Text>
-//           <Text>no downvote: {item.nodownvote}</Text>
-//           <Text>no comments: {item.nocomment}</Text>
-//         </View>);
+renderRow = ({item}) => 
+  (<View style={{backgroundColor:'green',margin:10}}>
+           <Image source={{uri:item.image}} style={{width:100,height:100,borderRadius:100}}/>
+           <Text>username: {item.username}</Text>
+           <Text>date: {item.date}</Text>
+           <Text>complain: {item.complain}</Text>
+           <Text>no upvote: {item.noupvote}</Text>
+           <Text>no downvote: {item.nodownvote}</Text>
+           <Text>no comments: {item.nocomment}</Text>
+  </View>);
 
  render() {
-   /*return <TouchableOpacity  onPress={()=>this.props.navigation.goBack()}>
-       <Text>Go to Screen 1</Text>
-     </TouchableOpacity>;*/
 
      return (
-      <View>
-        <TouchableOpacity onPress={()=>this.props.navigation.navigate('CommentPage',{age:29})}>
-          <Text>Go to Komen Page</Text>
-        </TouchableOpacity>
-        {/* <View style={{flex:1,paddingTop:100}}>
-            <Text>What is wrong?</Text>
+      <View style={{flex:1}}>
+          <View style={{flex:3, paddingTop:50}}>
             <FlatList
               data={database}
               renderItem={this.renderRow}
             />
-        </View> */}
-
-        <View style={{flex:1,paddingTop:100}}>
-            <Text>What is wrong?</Text>
-            <FlatList
-              data={data}
-              renderItem={this.renderItem}
-              keyExtractor={this.keyExtractor}
-            />
-        </View>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+            <TouchableOpacity style={{width: 100, height: 100, backgroundColor: 'blue'}} onPress={()=>this.props.navigation.navigate('Home',{age:29})}>
+              <Text>HOME</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{width: 100, height: 100, backgroundColor: 'red'}} onPress={()=>this.props.navigation.navigate('CommentPage',{age:29})}>
+              <Text>ADD COMPLAIN</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{width: 100, height: 100, backgroundColor: 'yellow'}} onPress={()=>this.props.navigation.navigate('Trending',{age:29})}>
+              <Text>TRENDING</Text>
+            </TouchableOpacity>
+          </View>
 
      </View>);
  }
